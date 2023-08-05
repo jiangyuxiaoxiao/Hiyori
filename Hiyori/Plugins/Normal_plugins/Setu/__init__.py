fdfa77dc64a95f7c036d1186363b2132fd926010
zaozi <2464106863@qq.com>
2023-08-05 15:30:09 +0800

    2023/08/05 15:30 修改setu插件，新增一个涩图接口。

--- Hiyori/Plugins/Normal_plugins/Setu/__init__.py
+++ Hiyori/Plugins/Normal_plugins/Setu/__init__.py
@@ -42,29 +42,33 @@ __plugin_meta__ = PluginMetadata(
 
 setu = on_regex(r"^(.*?)涩图$", permission=GROUP, priority=Priority.普通优先级)
 
-url = "https://api.lolicon.app/setu/v2"
+url1 = "https://liuliying.cn/api/setu"
+url2 = "https://api.lolicon.app/setu/v2"
 
 @setu.handle()
 async def _(matcher: Matcher, event: GroupMessageEvent):
     user_tag = re.search(r"^(.*?)涩图$", str(event.message)).group(1)
-    params = {
-        "r18": 0,  # 添加r18参数 0为否，1为是，2为混合
-        "tag": user_tag,  # 若指定tag
-        "num": 1,  # 一次返回的结果数量
-        "size": ["original"],
-    }
     try:
-        res = await AsyncHttpx.get(url=url, timeout=10000, params=params)
+        params = {
+            "tag": user_tag,  # 若指定tag
+        }
+        res = await AsyncHttpx.get(url=url1, timeout=10000, params=params)
         if res.status_code == 200:
             data = res.json()
-            if not data["error"]:
-                data = data["data"][0]
+            if data["url"]:
                 pid = data["pid"]
-                uid = data["uid"]
+                author = data["author"]
                 title = data["title"]
-                imgurl = data["urls"]["original"]
+                imgurl = data["url"]
+                try:
+                    imgurl = imgurl.replace('i.pixiv.cat','i.pixiv.re')
+                except:
+                    try:
+                        imgurl = imgurl.replace('i.pixiv.net', 'i.pixiv.re')
+                    except:
+                        imgurl = imgurl.replace('i.pximg.net', 'i.pixiv.re')
                 message = f'''
-                pid : {pid} \nuid : {uid} \ntitle: {title}
+                pid : {pid} \nauthor : {author} \ntitle: {title}
                 '''.strip()
                 message = message + MessageSegment.image(imgurl)
                 if not imgurl:
@@ -73,4 +77,49 @@ async def _(matcher: Matcher, event: GroupMessageEvent):
             else:
                 await setu.send("没找到符合条件的色图...")
     except:
-        await setu.send("没找到符合条件的色图...")
+        try:
+            params = {
+                "r18": 0,  # 添加r18参数 0为否，1为是，2为混合
+                "tag": user_tag,  # 若指定tag
+                "num": 1,  # 一次返回的结果数量
+                "size": ["original"],
+            }
+            res = await AsyncHttpx.get(url=url2, timeout=10000, params=params)
+            if res.status_code == 200:
+                data = res.json()
+                if not data["error"]:
+                    data = data["data"][0]
+                    pid = data["pid"]
+                    uid = data["uid"]
+                    r18 = data["r18"].capitalize()
+                    author = data["author"]
+                    tags = data["tags"]
+                    title = data["title"]
+                    imgurl = data["urls"]["original"]
+                    imageurl = imgurl.replace('i.pixiv.re','i.pixiv.cat')
+                    params = {
+                        "r18": r18,
+                        "tags": tags,
+                        "url":imageurl,
+                        "title":title,
+                        "pid":pid,
+                        "uid":uid,
+                        "author":author,
+                        "key":"请练习开发者"
+                    }
+                    res = await AsyncHttpx.get(url=url1, timeout=10000, params=params)
+                    if res.text == 'success':
+                        print("写入数据成功")
+                    else:
+                        print("写入数据失败，数据库回滚")
+                    message = f'''
+                    pid : {pid} \nauthor : {author} \ntitle: {title}
+                    '''.strip()
+                    message = message + MessageSegment.image(imgurl)
+                    if not imgurl:
+                        await setu.send("没找到符合条件的色图...")
+                    await setu.send(message, at_sender=True)
+                else:
+                    await setu.send("没找到符合条件的色图...")
+        except:
+            await setu.send("没找到符合条件的色图...")
