d9c3343e91bee0e49f91b77b0d3e00f9892264a6
Sora <atri@suzakuintsubaki.com>
2023-08-07 16:58:25 +0800

    2023/08/07 16:58 修正商店模块bug

--- Hiyori/Utils/Shop/__init__.py
+++ Hiyori/Utils/Shop/__init__.py
@@ -144,7 +144,7 @@ class Shops:
 
 def 折扣系数计算(QQ: int, ItemName: str) -> float:
     # if ItemName in Shops.shops["群友"].items.keys() or ItemName in Shops.shops["妃爱"].items.keys():
-    if ItemName in Shops.shops["妃爱"].items.keys():
+    if ItemName in Shops.shops["妃爱商店"].items.keys():
         if DB_Item.hasItem(QQ=QQ, ItemName="白银会员卡"):
             return 0.9
     return 1
