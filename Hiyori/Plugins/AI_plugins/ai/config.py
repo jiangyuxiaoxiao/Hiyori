69da055aa51c7c051495671ad8e710be799c88ad
zaozi <2464106863@qq.com>
2023-08-07 15:06:16 +0800

    2023/08/07 15:05 新增ai类插件ai消息回复.

---
+++ Hiyori/Plugins/AI_plugins/ai/config.py
@@ -0,0 +1,16 @@
+"""
+@Author: Ame lian
+@Github: https://github.com/AmeNeri
+@Date: 2023/8/7-12:48
+@Desc: 
+@Ver : 1.0.0
+"""
+from pathlib import Path
+
+TEXT_FILTER = ["鸡", "口交"]
+NICKNAME = ["妃爱酱"]
+DATA_PATH = Path() / "Data" / "ai"
+IMAGE_PATH = Path() / "Data" / "ai" / "images"
+TL_KEY = ""
+ALAPI_TOKEN = "OvYVzi93slKCLg4S"
+ALAPI_AI_CHECK = False
