8e5a2ecd4dc627f35d310db6700d80cf8108c9b7
Sora <atri@suzakuintsubaki.com>
2023-08-11 20:46:07 +0800

    2023/08/11 20:46 妃爱，再次启动！

--- 妃爱，启动！.bat
+++ 妃爱，启动！.bat
@@ -1,8 +1,14 @@
 if not exist .\venv\ (
     pip install virtualenv
+    virtualenv venv
+    call venv\Scripts\activate
     pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+    cd .\Hiyori
+    python hiyori.py
+    pause
+) else (
+    call venv\Scripts\activate
+    cd .\Hiyori
+    python hiyori.py
+    pause
 )
-call venv\Scripts\activate
-cd .\Hiyori
-python hiyori.py
-pause
